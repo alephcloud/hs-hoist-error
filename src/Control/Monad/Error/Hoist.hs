35918867b0ad463bfa9971d270f70bc30dfc067c
Mitchell Rosen <mitchellwrosen@gmail.com>
2015-11-28 18:25:24 -0800

    improve documentation

--- src/Control/Monad/Error/Hoist.hs
+++ src/Control/Monad/Error/Hoist.hs
@@ -4,6 +4,34 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
+-- | 'HoistError' extends 'MonadError' with 'hoistError', which enables lifting
+-- of partiality types such as 'Maybe' and @'Either' e@ into the monad.
+--
+-- For example, consider the following @App@ monad that may throw @BadPacket@
+-- errors:
+--
+-- @
+-- data AppError = BadPacket 'String'
+--
+-- newtype App a = App ('EitherT' AppError 'IO') a
+--  deriving ('Functor', 'Applicative', 'Monad', 'MonadError' AppError, 'MonadIO')
+-- @
+--
+-- We may have an existing function that parses a 'String' into a @'Maybe' Packet@
+--
+-- @
+-- parsePacket :: 'String' -> 'Maybe' Packet
+-- @
+--
+-- which can be lifted into the @App@ monad with 'hoistError'
+--
+-- @
+-- appParsePacket :: 'String' -> 'App' Packet
+-- appParsePacket s = 'hoistError' (\\() -> BadPacket "no parse") (parsePacket s)
+-- @
+--
+-- Similar instances exist for @'Either' e@ and @'EitherT' e m@.
+
 module Control.Monad.Error.Hoist
 ( HoistError(..)
 , (<%?>)
@@ -27,6 +55,11 @@ class Monad m ⇒ HoistError m t e e' | t → e where
   -- | Given a conversion from the error in @t α@ to @e'@, we can hoist the
   -- computation into @m@.
   --
+  -- @
+  -- hoistError :: 'MonadError' e m -> (() -> e) -> 'Maybe' a -> m a
+  -- hoistError :: 'MonadError' e m -> (a -> e) -> 'Either' a b -> m b
+  -- hoistError :: 'MonadError' e m -> (a -> e) -> 'EitherT' a m b -> m b
+  -- @
   hoistError
     ∷ (e → e')
     → t α
@@ -53,7 +86,6 @@ infixl 8 <%?>
 {-# INLINE (<%?>) #-}
 
 -- | A version of '<%?>' that operates on values already in the monad.
---
 (<%!?>)
   ∷ HoistError m t e e'
   ⇒ m (t α)
@@ -66,9 +98,8 @@ m <%!?> e = do
 infixl 8 <%!?>
 {-# INLINE (<%!?>) #-}
 
--- | A version of @hoistError@ that ignores the error in @t α@ and replaces it
+-- | A version of 'hoistError' that ignores the error in @t α@ and replaces it
 -- with a new one in @e'@.
---
 (<?>)
   ∷ HoistError m t e e'
   ⇒ t α
@@ -79,7 +110,7 @@ m <?> e = m <%?> const e
 infixl 8 <?>
 {-# INLINE (<?>) #-}
 
--- | A version of @<?>@ that operates on values already in the monad.
+-- | A version of '<?>' that operates on values already in the monad.
 (<!?>)
   ∷ HoistError m t e e'
   ⇒ m (t α)
